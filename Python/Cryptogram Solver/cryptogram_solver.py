cb36df89eed7e96903bca01288b3e56b2087b208
Nathan Lalli <nathan.lalli@eagles.oc.edu>
2022-11-18 02:20:54 -0600

    Remove some file locations

--- Python/Cryptogram Solver/cryptogram_solver.py
+++ Python/Cryptogram Solver/cryptogram_solver.py
@@ -12,7 +12,7 @@ dictionary = enchant.Dict("en")
 # message = input("Enter the cryptogram here: ")
 message = "ILDS KXJ SDSJCK UJ VLJ ZDLC, KXJ UDLQR KJCRW KD XVTJ V ZDE VQLJVRM ZGHQK IDL GW KD IHK HCWHRJ. DGL GSZHQHNVQ NDLR CJTJL WJJSW KD ZJ WJTJLJR; UJ DCQM IHCR CJU CJJRW KD IHQQ. HI UJ RHWNDCCJNKJR VCR WJTJLJR DGL VKKVNXSJCKW, UDGQR UJ WXVKKJL DGL NDCIHCJSJCKW VCR JEAVCR ZJMDCR DGL WXJQQ? UDGQR KXJ UDLQR QDDY RHIIJLJCK? UDGQR UJ LJNDPCHBJ DGLWJQTJW? VLJ"
 
-with open(os.getcwd() + "/Python/Cryptogram Solver/EN.json") as fh:
+with open(os.getcwd() + "/EN.json") as fh:
     solver = subbreaker.Breaker(fh)
 
 while secret:
