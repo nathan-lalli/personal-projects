ca7eb21047192ab6df6f53b65c7856cd13953cec
Nathan Lalli <nathan.lalli@eagles.oc.edu>
2022-11-18 12:11:53 -0600

    Adds a folder for batch Files
    Creates the print_nightmare.bat to fix the print_nightmare bug
    in Windows 10 and 11.

---
+++ Batch Files/print_nightmare.bat
@@ -0,0 +1,13 @@
+:: Running this will allow a user to add a printer to their device after it has been affected by the 
+:: Print Nightmare bug in Windows 10 and 11
+
+:: adds the key to allow any user to do any driver installations
+reg add "HKEY_LOCAL_MACHINE\\Software\\Policies\\Microsoft\\Windows\\DriverInstall\\Restrictions" /v AllowUserDeviceClasses /t REG_DWORD /d 1 /f
+
+:: adds the keys to disabled the point and print restrictions
+reg add "HKEY_LOCAL_MACHINE\\Software\\Policies\\Microsoft\\Windows NT\\Printers\\PointAndPrint" /v InForest /t REG_DWORD /d 0 /f
+reg add "HKEY_LOCAL_MACHINE\\Software\\Policies\\Microsoft\\Windows NT\\Printers\\PointAndPrint" /v Restricted /t REG_DWORD /d 0 /f
+reg add "HKEY_LOCAL_MACHINE\\Software\\Policies\\Microsoft\\Windows NT\\Printers\\PointAndPrint" /v TrustedServers /t REG_DWORD /d 0 /f
+
+:: adds the key to not restrict driver installation to only administrators
+reg add "HKEY_LOCAL_MACHINE\\Software\\Policies\\Microsoft\\Windows NT\\Printers\\PointAndPrint" /v RestrictDriverInstallationToAdministrators /t REG_DWORD /d 0 /f
