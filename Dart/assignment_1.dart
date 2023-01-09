b70144671a9e4402f56a982cf6d69691ab50897c
Nathan Lalli <nathan.lalli@eagles.oc.edu>
2023-01-09 14:10:33 -0600

    Adds the first assignment and starts work on it

---
+++ Dart/assignment_1.dart
@@ -0,0 +1,45 @@
+// Assignment 1: Write a program that incorporates variables, loops, and at least 1 class.
+
+import 'package:list_english_words/list_english_words.dart';
+
+class CharacterCreator {
+  String gender, firstname, lastname, species;
+
+  void set setgender(gender) {
+    this.gender = gender;
+  }
+
+  void set setfirstname(firstname) {
+    this.firstname = firstname;
+  }
+
+  void set setlastname(lastname) {
+    this.lastname = lastname;
+  }
+
+  void set setspecies(species) {
+    this.species = species;
+  }
+
+  String get getgender {
+    return this.gender;
+  }
+
+  String get getfirstname {
+    return this.firstname;
+  }
+
+  String get getlastname {
+    return this.lastname;
+  }
+
+  String get getspecies {
+    return this.species;
+  }
+}
+
+main() {
+  // Create a list of 5 characters to choose from
+
+  // Word list for names and species
+}
