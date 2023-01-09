b09f2f277baec303dc13c228e2392d3f295b1d4f
Nathan Lalli <nathan.lalli@eagles.oc.edu>
2023-01-09 17:41:10 -0600

    Full functionality
    Makes a list of 5 random characters to select with random attributes

--- Dart/assignment_1.dart
+++ Dart/assignment_1.dart
@@ -1,23 +1,20 @@
 // Assignment 1: Write a program that incorporates variables, loops, and at least 1 class.
 
-import 'package:list_english_words/list_english_words.dart';
+import 'package:rpgnamegenerator/rpgnamegenerator.dart';
+import 'dart:math';
 
 class CharacterCreator {
-  String gender, firstname, lastname, species;
+  var gender, name, species;
 
-  void set setgender(gender) {
+  void setgender(gender) {
     this.gender = gender;
   }
 
-  void set setfirstname(firstname) {
-    this.firstname = firstname;
+  void setname(name) {
+    this.name = name;
   }
 
-  void set setlastname(lastname) {
-    this.lastname = lastname;
-  }
-
-  void set setspecies(species) {
+  void setspecies(species) {
     this.species = species;
   }
 
@@ -25,12 +22,8 @@ class CharacterCreator {
     return this.gender;
   }
 
-  String get getfirstname {
-    return this.firstname;
-  }
-
-  String get getlastname {
-    return this.lastname;
+  String get getname {
+    return this.name;
   }
 
   String get getspecies {
@@ -40,6 +33,32 @@ class CharacterCreator {
 
 main() {
   // Create a list of 5 characters to choose from
-
   // Word list for names and species
+
+  var randomnumber = Random();
+  List<String> genders = ["m", "f"];
+  List<String> species = ['orc', 'elf', 'human'];
+  List<CharacterCreator> characters = [];
+
+  for (int i = 0; i < 5; i++) {
+    var character = CharacterCreator();
+    character.setgender(genders[randomnumber.nextInt(2)]);
+    character.setspecies(species[randomnumber.nextInt(3)]);
+    character.setname(
+        RpgNameGenerator.getName(character.getspecies, character.getgender));
+    characters.add(character);
+  }
+
+  // Print out the list of characters
+  for (int i = 0; i < 5; i++) {
+    var option = i + 1;
+    print("Character " +
+        option.toString() +
+        ": " +
+        characters[i].getname +
+        ", " +
+        characters[i].getgender +
+        ", " +
+        characters[i].getspecies);
+  }
 }
