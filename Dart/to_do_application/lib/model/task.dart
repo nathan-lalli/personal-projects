b8110b59bef98df7a2a87fc73a29a11e6c11d092
Nathan Lalli <nathan.lalli@eagles.oc.edu>
2023-02-01 16:58:34 -0600

    commit

--- Dart/to_do_application/lib/model/task.dart
+++ Dart/to_do_application/lib/model/task.dart
@@ -1,11 +1,24 @@
 import 'package:uuid/uuid.dart';
 
 class Task {
-  Task({this.description = '', String? id, required taskStatus})
-      : isCompleted = false,
+  Task({this.description = '', String? id, bool? taskStatus})
+      : taskStatus = false,
         id = id ?? _uuid.v1();
-  bool isCompleted;
   static const _uuid = Uuid();
   final String description;
   final String id;
+  final bool taskStatus;
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'description': description,
+      'taskStatus': taskStatus,
+    };
+  }
+
+  @override
+  String toString() {
+    return 'Task{id: $id, description: $description, taskStatus: $taskStatus}';
+  }
 }
--- Dart/to_do_application/lib/services/storage.dart
+++ Dart/to_do_application/lib/services/storage.dart
@@ -3,7 +3,7 @@ import '../model/task.dart';
 abstract class Storage {
   Future<List<Task>> getTasks();
 
-  Future<Task> insertTask(String description, bool taskStatus);
+  Future<void> insertTask(String description, bool taskStatus);
 
   Future<void> removeTask(Task task);
 }
