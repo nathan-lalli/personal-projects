85ff799a9d45ca1c99cec04efb8ccb5dd3b751ab
Nathan Lalli <nathan.lalli@eagles.oc.edu>
2023-01-31 20:44:06 -0600

    Finished HW3

---
+++ Dart/hw3/lib/secondary_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+  final int value;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
