205da91c11deae6d8415da5db5cb4941c6733096
Nathan Lalli <nathan.lalli@eagles.oc.edu>
2023-05-16 19:33:50 -0500

    Removeing all Dart files

--- Dart/to_do_application/android/app/src/main/kotlin/com/lalli_nathan/to_do_application/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.lalli_nathan.to_do_application
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Dart/weather-app-assignment/android/app/src/main/kotlin/org/happycyclops/oc_weather_assignment/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package org.happycyclops.oc_weather_assignment
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
